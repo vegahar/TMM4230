d3fd4a30223ed4d52dcac6f7c9882f116396644c
Vegard Harper <vegard.harper@gmail.com>
2013-04-16 23:13:41 +0200

    Added person, list and service

---
+++ Server/PersonSearch/PersonSearch/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PersonSearch
+{
+    public class Person
+    {
+        string firstName;
+        string lastName;
+        int age;
+
+        public Person(string firstname, string lastname, int age)
+        {
+            this.firstName = firstname;
+            this.lastName = lastname;
+            this.age = age;
+        }
+
+        public string Name
+        {
+            get {return firstName + " " + lastName; }
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public string LasttName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+    }
+}
---
+++ Server/PersonSearch/PersonSearch/PersonList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PersonSearch
+{
+    public class PersonList
+    {
+
+        List<Person> list = new List<Person>();
+
+
+    }
+}
